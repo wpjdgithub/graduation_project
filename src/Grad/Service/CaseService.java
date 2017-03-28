d5a019c6322dd9486255412b645c8c307e498072
wpjdgithub <wpjdmhxy2@163.com>
2017-03-28 16:10:13 +0800

    上传文件

--- src/Grad/Service/CaseService.java
+++ src/Grad/Service/CaseService.java
@@ -1,11 +1,14 @@
 package Grad.Service;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import Grad.Bean.CaseDetail;
 import Grad.Bean.CaseMinMes;
 
 public interface CaseService {
+	public boolean uploadCase(String username, InputStream in);
 	public ArrayList<CaseMinMes> getCaseByUser(String username); // 获取用户个人上传文书
+	
 	public CaseDetail getCaseByTitle(String title); // 依据文书标题获取全部信息
 }
